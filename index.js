b7ffb8c35c19e88f2dccfd882aa1541164403bc4
Mukesh16407 <mukesh.16407@gmail.com>
2024-05-09 20:22:47 +0530

    fixed cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dbConfig = require("./config/dbConfig");
 require("dotenv").config();
+const cors = require("cors");
 
 const userRoute = require("./routes/userRoutes");
 const distributeRoutes = require("./routes/distributeRoutes");
@@ -11,6 +12,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use("/api", userRoute);
